7627a04a944e737c4399b5d8a1fb6e9a62a9ccff
Jeff-00 <71959298+Jeff-00@users.noreply.github.com>
2020-11-28 16:29:59 +0900

    Create score.py

---
+++ score.py
@@ -0,0 +1,48 @@
+#S
+for i in range(2,5,2):
+    LMD.set_pixel(0,i,3)
+
+LMD.set_pixel(1,1,3)
+LMD.set_pixel(1,2,3)
+LMD.set_pixel(1,4,3)
+
+for i in range(1,4,2):
+    LMD.set_pixel(2,i,3)
+
+#C
+for i in range(2,4):
+    LMD.set_pixel(4,i,3)
+
+for i in range(1,5,3):
+    LMD.set_pixel(5,i,3)
+
+for i in range(1,5,3):
+    LMD.set_pixel(6,i,3)
+
+#O
+for i in range(2,4):
+    LMD.set_pixel(8,i,3)
+
+for i in range(1,5,3):
+    LMD.set_pixel(9,i,3)
+
+for i in range(2,4):
+    LMD.set_pixel(10,i,3)
+
+#R
+for i in range(1,5):
+    LMD.set_pixel(12,i,3)
+
+LMD.set_pixel(13,2,3)
+LMD.set_pixel(14,1,3)
+
+#e
+for i in range(2,4):
+    LMD.set_pixel(16,i,3)
+
+LMD.set_pixel(17,1,3)
+LMD.set_pixel(17,3,3)
+LMD.set_pixel(17,4,3)
+
+for i in range(2,5,2):
+    LMD.set_pixel(18,i,3)
